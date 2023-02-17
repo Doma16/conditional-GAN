e09cb7dcb4c0c6717c48f1ccba1a3148ef18cf44
domaSaric <ds53234@fer.hr>
2023-02-17 01:12:52 +0100

    model idea

---
+++ model.py
@@ -0,0 +1,33 @@
+import torch
+import torch.nn as nn
+
+class Discriminator(nn.Module):
+    def __init__(self, in_channels, features, img_size) -> None:
+        super(Discriminator, self).__init__()
+        self.disc = nn.Sequential(
+
+        )
+
+    def block():
+        return nn.Sequential(
+            
+        )
+
+    def forward(self, x, label):
+        pass
+
+
+class Generator(nn.Module):
+    def __init__(self, vect_dim, features, out_channels) -> None:
+        super(Generator, self).__init__()
+        self.gen = nn.Sequential(
+
+        )
+
+    def block(self, ):
+        return nn.Sequential(
+
+        )
+
+    def forward(self, x, label):
+        pass
